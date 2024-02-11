5a42776f4432dee99529fb89e0fdf3467a4d49c5
m0thman70 <dominiksarnowicz@gmail.com>
2024-02-11 23:47:32 +0000

    home

--- flake.nix
+++ flake.nix
@@ -21,8 +21,15 @@
       "itsreproducibleguys" = nixpkgs.lib.nixosSystem {
         inherit specialArgs;
         system = "x86_64-linux";
-        modules = [./configuration.nix];
+        modules = [./configuration.nix ./modules/nixos];
       };
     };
+    homeConfigurations = {
+      "dom@itsreproducibleguys" = home-manager.lib.homeManagerConfiguration {
+        extraSpecialArgs = specialArgs;
+        inherit pkgs;
+	modules = [./modules/home ./home];
+      };
+   };
   };
 }
--- home/default.nix
+++ home/default.nix
@@ -1,4 +1,7 @@
-{
-  imports = [
-  ];
+{ pkgs, ... }: {
+  home = rec {
+    username = "dom";
+    homedirectory = "/home/${username}";
+    stateversion = "23.11";
+  };
 }
